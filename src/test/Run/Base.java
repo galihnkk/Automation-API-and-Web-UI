045472c3276f86c9c9f1ad6b9f58a0a9a50d5ee9
Galih Nur Khalis Kasyfi <galihnkk96@gmail.com>
2024-09-27 15:40:34 +0700

    Update Base.java

--- src/test/Run/Base.java
+++ src/test/Run/Base.java
@@ -8,14 +8,14 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class Base {
 
     protected static WebDriver driver;
+    
     protected void getDriver() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm=usage");
         options.addArguments("--remote-allow=origin=*");
         options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
     }
 }
